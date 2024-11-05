49e8bda3d1ab6b0a7a5aed613d640aef8b4ba41b
Edgar Modesto <edgarmodesto23@gmail.com>
2024-11-04 17:44:35 -0700

    codecrafters submit [skip ci]

--- app/versions.go
+++ app/versions.go
@@ -9,25 +9,46 @@ type APIVersions struct {
 	request Request
 }
 
+func writeAPIKeysSupportedNum(buf *bytes.Buffer){
+  buf.WriteByte(byte(3))
+}
+
+func supportApiKey18(buf *bytes.Buffer) error {
+  body := []int16{18, 3, 4}
+  for _, v := range body {
+    if err := binary.Write(buf, binary.BigEndian, v); err != nil {
+      return err
+    }
+  }
+
+  buf.WriteByte(byte(0))
+  
+  return nil
+}
+
+func supportApiKey75(buf *bytes.Buffer) error{
+  body := []int16{75,0,4}
+
+  for _, v := range body {
+    if err := binary.Write(buf, binary.BigEndian, v); err != nil {
+      return err
+    }
+  }
+
+  buf.WriteByte(byte(0))
+
+  return nil
+}
+
 func (h *APIVersions) Execute() Response {
-	body := []int16{18, 3, 4}
 
 	buf := new(bytes.Buffer)
 
-  buf.WriteByte(byte(2))
+  writeAPIKeysSupportedNum(buf)
 
-	for _, v := range body {
-		if err := binary.Write(buf, binary.BigEndian, v); err != nil {
-      return Response{
-				corr_id:  h.request.headers.corr_id,
-				err_code: -1,
-				body:     []byte{},
-				length:   6,
-			}
-		}
-	}
+  supportApiKey18(buf)
+  supportApiKey75(buf)
 
-  buf.WriteByte(byte(0))
   buf.Write([]byte{0,0,0,0})
   buf.WriteByte(byte(0))
 
